ae2e92976be5d0239c3907c4b3d9331919775234
Hüdayi Aşık <78505640+hudayiasik@users.noreply.github.com>
2024-03-16 02:12:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from app.handlers import MyHandler
 
-def run_server(host='192.168.1.65', port=8000):
+def run_server(host='192.168.1.65', port=8000): # change host address to your  ip
     server_address = (host, port)
     httpd = HTTPServer(server_address, MyHandler)
     print(f"Server running on http://{host}:{port}")
